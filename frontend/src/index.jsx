b8bc0d15fd9f3e889a8547657cd40f435c34662c
NaBa26 <namankumar777@gmail.com>
2024-05-13 12:05:05 +0530

    New Commit

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./header";
+import Slideshow from "./imageslideshow";
 
 function App() {
 return (
   <>
   <Header />
+  <Slideshow />
   </>
 )
 }
--- frontend/src/slides.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Slide(){
-
-}
-
-export default Slide;
